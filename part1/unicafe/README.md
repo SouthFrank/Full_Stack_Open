530fdefa7d052579a599471326f034b236c4ef70
Frank South <frank@franksouth.dev>
2023-12-23 10:29:50 -0500

    Exercise 1.6 completed, README updated, eslint rules changed to avoid false errors

--- part1/unicafe/README.md
+++ part1/unicafe/README.md
@@ -1,2 +1,8 @@
 # Part 1 Introduction to React
 This project contains Exercises 1.6 to 1.14. Exercises 1.1 to 1.5 can found in the other folder within the 'part1' section of this repository, named 'courseinfo'.
+
+## Exercise 1.6
+- Your task is to implement a web application for collection customer feedback. There are only three options for feedback: good, neutral, and bad.
+- The application must display the total number of collected feedback for each category. 
+
+Status: *completed*
--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const Display = ({text, count}) => (
+  <p>{text} {count}</p>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +16,15 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      
+      <h2>Statistics</h2>
+      <Display text='Good' count={good} />
+      <Display text='Neutral' count={neutral} />
+      <Display text='Bad' count={bad} />
     </div>
   )
 }
