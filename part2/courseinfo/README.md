b51ce640dd1a3f3804ee3e1deb3302a2cc75c900
Frank South <frank@franksouth.dev>
2023-12-27 10:32:18 -0500

    Exercises 2.1 and 2.2 completed, eslint updated to remove error

--- part2/courseinfo/README.md
+++ part2/courseinfo/README.md
@@ -1,5 +1,20 @@
 # Part 2 Communicating with the Server - Course Info
 
-- This project picks up where the 'courseinfo' project in part1 left off, at the end of Exercise 1.5
+- This project picks up where the 'courseinfo' project in part1 left off, at the end of Exercise 1.5.
 
 ## Exercise 2.1
+
+- Define a component responsible for formatting a single course called Course.
+- Make sure to write the component such that it will work, no matter how many items are in the array.
+- Ensure that the console shows no errors.
+- The array method 'map' was used to achieve this.
+
+Status: _completed_
+
+## Exercise 2.2
+
+- Show also the sum of all of the exercises.
+- Make sure to write the component such that it will work, no matter how many items are in the array.
+- The array method 'reduce' was used to achieve this.
+
+Status: _completed_
--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,67 +1,37 @@
-const Header = (props) => {
-  const courseName = props.course.name
-  return (
+const Course = ({course}) => {
+  return(
     <div>
-      <h1>{courseName}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  // console.log(props)
-  return (
-    <>
-      <p>{props.part} {props.exercises}</p>
-    </>
-  )
-  
-}
-
-const Content = (props) => {
-  const courseParts = props.course.parts
-  return (
-    <div>
-      <Part part={courseParts[0].name} exercises={courseParts[0].exercises}/>
-      <Part part={courseParts[1].name} exercises={courseParts[1].exercises}/>
-      <Part part={courseParts[2].name} exercises={courseParts[2].exercises}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const courseParts = props.course.parts
-  return (
-    <div>
-      <p>The total number of courses is: {courseParts[0].exercises + courseParts[1].exercises + courseParts[2].exercises}</p>
+      <h1>{course.name}</h1>
+      {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+      <h5>There are a total of {course.parts.reduce((sum, current) => sum + current.exercises, 0)} courses</h5>
     </div>
   )
 }
 
 const App = () => {
-const course = {
-  name: 'Half Stack application development',
-  parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course}/>
-      <Total course={course}/>
-    </div>
-  )
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
