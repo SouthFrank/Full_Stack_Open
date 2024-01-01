36ff4e70d6245afb7c9b7a2e1dd88282294ad0d6
Frank South <frank@franksouth.dev>
2024-01-01 13:05:58 -0500

    Completed Exercise 2.9

--- part2/phonebook/README.md
+++ part2/phonebook/README.md
@@ -1,8 +1,32 @@
-# React + Vite
+# Part 2 Communicating with the Server - The Phonebook
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- This marks the beginning of a new project, 'The Phonebook'. Exercises 2.6 to 2.10 focus on submitting and accessing data in forms, with the use of state changes and event handlers.
 
-Currently, two official plugins are available:
+## Exercise 2.6
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Create a simple Phonebook.
+- In this section we will only be adding names to the phonebook.
+
+Status: _completed_
+
+## Exercise 2.7
+
+- Prevent the user from being able to add names that already exist in the phonebook. 
+- Issue a warning with the alert command when such an action is attempted.
+- Array method 'every()' was used to avoid duplicate names being allowed.
+
+Status: *completed*
+
+## Exercise 2.8
+
+- Expand your application by allowing users to add phone numbers to the phone book.
+
+Status: *completed*
+
+## Exercise 2.9
+
+- Enter a search field that can be used to filter the list of people based on name. 
+- Case insensitive search.
+- Changed what is displayed in the Contact List.
+
+Status: *completed*
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,47 +1,84 @@
 import { useState } from 'react'
 import './App.css'
 
-const Person = ({name}) => {
+const Person = ({name, number}) => {
   return (
-    <li>{name}</li>
+    <li>{name} - {number}</li>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
-  const [newName, setNewName] = useState('Frank')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+  const [newName, setNewName] = useState('Please enter your name')
+  const [newNumber, setNewNumber] = useState('Please enter your number')
+  const [filterValue, setFilterValue] = useState()
 
-  const addName = (event) => {
+  const filteredList = !filterValue ? persons : persons.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()))
+
+  const addPerson = (event) => {
     event.preventDefault()
-    const nameObject = {
-      name: newName,
-      id: persons.length + 1
+
+    const nameCheck = (person) => person.name !== newName
+
+    if(persons.every(nameCheck)){
+      const nameObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1
+      }
+  
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+      setNewNumber('')
+    } else {
+      alert(`The name '${newName}' is already in use. Please change.`)
     }
 
-    setPersons(persons.concat(nameObject))
-    setNewName('')
+    
     console.log(persons)
   }
 
+
+  //Event Handler Functions
   const handleNameChange = event => setNewName(event.target.value)
+
+  const handleNumberChange = event => setNewNumber(event.target.value)
+
+  const handleFilterChange = event => setFilterValue(event.target.value)
   
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <h1>Phonebook</h1>
+      <form onChange={handleFilterChange}>
+        <div>
+          Search Phonebook by Name: <input />
+        </div>
+      </form>
+
+      <h2>Add a Contact</h2>
+      <form onSubmit={addPerson}>
         <div>
           Name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          Number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+
+      <h2>Contact List</h2>
       <div>  
         <ul>
-          {persons.map(person => 
-            <Person key={person.id} name={person.name}/>
+          {filteredList.map(person => 
+            <Person key={person.id} name={person.name} number={person.number} />
           )} 
         </ul>      
       </div>
