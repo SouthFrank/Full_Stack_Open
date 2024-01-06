8a3555f12c9df5170b6c847820731fa277f347c4
Frank South <frank@franksouth.dev>
2024-01-06 16:09:49 -0500

    Completed Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 const Filter = ({handleFilter}) => {
@@ -47,15 +48,18 @@ const Person = ({name, number}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('Please enter your name')
   const [newNumber, setNewNumber] = useState('Please enter your number')
   const [filterValue, setFilterValue] = useState()
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const filteredList = !filterValue ? persons : persons.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()))
 
